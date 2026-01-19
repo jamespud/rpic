24b2cc98f68719d2b07f4a9646e4a3b74b501e1b
spud <92900806+jamespud@users.noreply.github.com>
2026-01-19 15:12:06 +0800

    fix: improve error handling in RpcClientHandler and enhance ServiceURL parameter formatting

--- rpic-spring-boot-starter/src/main/java/com/spud/rpic/util/CommonUtils.java
+++ rpic-spring-boot-starter/src/main/java/com/spud/rpic/util/CommonUtils.java
@@ -14,14 +14,30 @@ public class CommonUtils {
 
 	public static ServiceMetadata buildServiceMetadata(RpcService annotation, Object bean) {
 		// 获取接口名称
-		String interfaceName = StringUtils.hasText(annotation.interfaceName())
-			? annotation.interfaceName()
-			: bean.getClass().getInterfaces()[0].getName();
+		String interfaceName;
+		if (StringUtils.hasText(annotation.interfaceName())) {
+			interfaceName = annotation.interfaceName();
+		} else {
+			Class<?>[] interfaces = bean.getClass().getInterfaces();
+			if (interfaces != null && interfaces.length > 0) {
+				interfaceName = interfaces[0].getName();
+			} else {
+				throw new IllegalArgumentException("@RpcService missing interface and no implemented interfaces found on bean: " + bean.getClass().getName());
+			}
+		}
 
 		// 获取接口类
-		Class<?> interfaceClass = annotation.interfaceClass() != void.class
-			? annotation.interfaceClass()
-			: bean.getClass().getInterfaces()[0];
+		Class<?> interfaceClass;
+		if (annotation.interfaceClass() != void.class) {
+			interfaceClass = annotation.interfaceClass();
+		} else {
+			Class<?>[] interfaces = bean.getClass().getInterfaces();
+			if (interfaces != null && interfaces.length > 0) {
+				interfaceClass = interfaces[0];
+			} else {
+				throw new IllegalArgumentException("@RpcService missing interface class and no implemented interfaces found on bean: " + bean.getClass().getName());
+			}
+		}
 
 		return ServiceMetadata.builder()
 			.interfaceName(interfaceName)
@@ -45,9 +61,14 @@ public class CommonUtils {
 	}
 
 	private static String getServiceName(RpcService annotation, Object bean) {
-		return StringUtils.hasText(annotation.interfaceName())
-			? annotation.interfaceName()
-			: bean.getClass().getInterfaces()[0].getName();
+		if (StringUtils.hasText(annotation.interfaceName())) {
+			return annotation.interfaceName();
+		}
+		Class<?>[] interfaces = bean.getClass().getInterfaces();
+		if (interfaces != null && interfaces.length > 0) {
+			return interfaces[0].getName();
+		}
+		throw new IllegalArgumentException("@RpcService missing interface and no implemented interfaces found on bean: " + bean.getClass().getName());
 	}
 
 	private static String getMethodServiceName(RpcService annotation, Method method) {
@@ -57,9 +78,14 @@ public class CommonUtils {
 	}
 
 	private static Class<?> getServiceInterface(RpcService annotation, Object bean) {
-		return annotation.interfaceClass() != void.class
-			? annotation.interfaceClass()
-			: bean.getClass().getInterfaces()[0];
+		if (annotation.interfaceClass() != void.class) {
+			return annotation.interfaceClass();
+		}
+		Class<?>[] interfaces = bean.getClass().getInterfaces();
+		if (interfaces != null && interfaces.length > 0) {
+			return interfaces[0];
+		}
+		throw new IllegalArgumentException("@RpcService missing interface class and no implemented interfaces found on bean: " + bean.getClass().getName());
 	}
 
 	public static ServiceMetadata buildConsumerMetadata(RpcReference reference, Class<?> type) {
